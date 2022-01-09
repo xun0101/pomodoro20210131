c4210b7d3ad86ca073eb3a90df4bf12acd9a0135
xun0101 <dabby6521@gmail.com>
2022-01-10 00:08:16 +0800

    20220109

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,18 @@
     b-row.w-75
       b-col.d-flex.flex-column.justify-content-between.align-items-center
         .time.h-100.w-100.d-flex.justify-content-center
-          span {{ timeText }}
-        .d-flex.align-items-center.justify-content-around.mt-5.w-50
-          button.btn.rounded-circle.btn-outline-light.bell
+          span
+        .d-flex.align-items-center.justify-content-center.mt-5.w-50
+          button.btn.rounded-circle.btn-outline-light.bell.mx-3
             img.img-fluid(src='../assets/image/icon-bell.svg')
-          button.btn.rounded-circle.play(v-if='status !== 1' @click='start')
+          button.btn.rounded-circle.play.mx-3(v-if='status !== 1' @click='start')
             img(src='../assets/image/icon-play--orange.svg')
-          button.btn.rounded-circle.play(v-else @click='pause')
+          button.btn.rounded-circle.play.mx-3(v-else @click='pause')
             img(src='../assets/image/icon-play--green.svg')
-          button.btn.rounded-circle.btn-outline-light.bell
+          button.btn.rounded-circle.btn-outline-light.bell.mx-3
             img.img-fluid(src='../assets/image/icon-delete.svg')
       b-col.text
-        h1 {{ currentText }}
+        h1 休息
     img.tomato(src='../assets/image/tomato--orange.svg')
 </template>
 <!-- <style>
@@ -72,65 +72,5 @@ opacity: 1;
 
 <script>
 export default {
-  data () {
-    return {
-      // 0 = 停止
-      // 1 = 倒數中
-      // 2 = 暫停
-      status: 0,
-      timer: 0
-    }
-  },
-  computed: {
-    current () {
-      return this.$store.state.current
-    },
-    items () {
-      return this.$store.state.items
-    },
-    timeleft () {
-      return this.$store.state.timeleft
-    },
-    timeText () {
-      const m = Math.floor(this.timeleft / 60).toString().padStart(2, '0')
-      const s = Math.floor(this.timeleft % 60).toString().padStart(2, '0')
-      return `${m} : ${s}`
-    }
-  },
-  methods: {
-    start () {
-      if (this.status === 0 && this.items.length > 0) {
-        this.$store.commit('start')
-      }
-      if (this.current.length) {
-        this.status = 1
-        this.timer = setInterval(() => {
-          this.$store.commit('countdown')
-          if (this.timeleft <= -1) {
-            this.finish(false)
-          }
-        }, 1000)
-      }
-    },
-    pause () {
-      this.status = 2
-      clearInterval(this.timer)
-    },
-    finish (skip) {
-      clearInterval(this.timer)
-      this.status = 0
-      this.$store.commit('finish')
-
-      if (!skip) {
-        const audio = new Audio()
-        audio.src = require('@/assets/' + this.$store.state.sound)
-        audio.play()
-      }
-
-      if (this.items.length > 0) {
-        this.start()
-      }
-    }
-  }
 }
 </script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,39 +1,72 @@
 <template>
   <div id="list" class="d-flex w-100">
-<div class="d-flex flex-column justify-content-between w-100 align-items-center mt-5">
-    <b-row class="w-100">
-      <b-col class="bg-danger">
-        <h1>待辦清單</h1>
-        <input type="text" class="w-100 addinput">
-        <button class="add-btn"><img src="../assets/image/icon-cancel.svg" class="add img-fluid"></button>
-      </b-col>
-      <b-col class="d-flex flex-column justify-content-between align-items-center">
-        <div class="time h-100 w-100 d-flex justify-content-center">
-        <span>{{ timeText }}</span>
+<div class="d-flex flex-column justify-content-between  w-100 p-5">
+    <b-row class="heihgt">
+      <b-col cols="4">
+        <div class="d-flex justify-content-between align-items-center">
+        <h1 class="h1text">待辦清單</h1>
+        <div class="d-flex w-25 justify-content-between">
+        <label class="input-text" for="undone">
+        <input id="undone" class="d-none" name="list" value="undone" type="radio" checked @click="num = 1">
+        未完成
+        </label>
+        <label class="input-text" for="completed">
+        <input id="completed" class="d-none" name="list" value="completed" type="radio" @click="num = 2">
+        已完成
+        </label>
         </div>
-        <div class="d-flex align-items-center justify-content-around mt-5 w-50">
-          <button class="btn rounded-circle btn-outline-light bell"><img class="img-fluid" src="../assets/image/icon-bell.svg"></button>
-          <button  v-if="status !== 1" @click="start" class="btn rounded-circle play"><img src="../assets/image/icon-play--orange.svg"></button>
-          <button  v-else @click="pause" class="btn rounded-circle play"><img src="../assets/image/icon-play--green.svg"></button>
-          <button  class="btn rounded-circle btn-outline-light bell"><img class="img-fluid" src="../assets/image/icon-delete.svg"></button>
         </div>
+        <div class="search">
+          <input type="text" placeholder="新增待辦事項" class="addinput p-3">
+          <button class="add-btn"><img src="../assets/image/icon-cancel.svg" class="add img-fluid"></button>
+        </div>
+        <ul v-if="num == 1" class="list-group list-group-flush mt-5">
+        <li class="d-flex align-items-center border-top pt-3">
+          <div class="mr-3 undonecheck"></div>
+          <div class="mr-auto undonetext list-item">13</div>
+          <button class="edit"><img src="../assets/image/icon-edit.svg"></button>
+          <button class="edit"><img src="../assets/image/icon-cancel.svg"></button>
+        </li>
+        </ul>
+        <ul v-else class="list-group list-group-flush mt-5">
+        <li class="d-flex align-items-center border-top pt-3">
+          <div class="mr-3 undonecheck text-white text-center">&#10003;</div>
+          <div class="mr-auto undonetext list-item">456</div>
+          <button class="edit"><img src="../assets/image/icon-cancel.svg"></button>
+        </li>
+        </ul>
+        <hr id="hr">
       </b-col>
-      <b-col class="text">
-        <h1>{{ currentText }}</h1>
+      <b-col class="d-flex flex-column justify-content-between">
+        <div class="d-flex flex-column align-items-center">
+        <div class="time w-100 d-flex justify-content-center">
+        <span>25:00</span>
+        </div>
+        <div class="d-flex align-items-center justify-content-center mt-5 w-50">
+          <button class="btn rounded-circle btn-outline-light bell mx-3"><img class="img-fluid" src="../assets/image/icon-bell.svg"></button>
+          <button class="btn rounded-circle play mx-3"><img src="../assets/image/icon-play--orange.svg"></button>
+          <button class="btn rounded-circle play mx-3"><img src="../assets/image/icon-play--green.svg"></button>
+          <button  class="btn rounded-circle btn-outline-light bell mx-3"><img class="img-fluid" src="../assets/image/icon-delete.svg"></button>
+        </div>
+        </div>
       </b-col>
     </b-row>
-    <img class="tomato" src="../assets/image/tomato--orange.svg" >
+    <div class="half"><img class="img" src="../assets/image/tomato--orange.svg" ></div>
   </div>
   </div>
 </template>
 
 <style>
+.heihgt{
+  height: 100vh;
+}
 .add{
   transform:rotate(45deg);
 }
 .add-btn{
-  top: 187px;
-left: 624px;
+  position: absolute;
+  top: 10%;
+left: 90%;
 width: 48px;
 height: 40px;
 background: #F08448 0% 0% no-repeat padding-box;
@@ -42,17 +75,111 @@ opacity: 1;
 border: none;
 }
 
-.appinput{
-  top: 179px;
-left: 120px;
-width: 560px;
+.addinput{
+width: 100%;
 height: 56px;
 background: #FFFFFF 0% 0% no-repeat padding-box;
 border: 1px solid #FFFFFF;
 border-radius: 183px;
 opacity: 1;
 }
+
+.search{
+  width: 560px;
+  position: relative;
+}
+
+.half{
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  object-position: right bottom;
+  width: 720px;
+  overflow: hidden;
+}
+#hr{
+  color: #000;
+    border-top: 1px solid rgba(0,0,0,.1);
+    box-shadow: inset 0 10px 10px -10px;
+  -webkit-transform: rotate(270deg);
+  width: 100vh;
+  height: 2px;
+  border: 0;
+  padding-top: 10px;
+  position: absolute;
+  left: 20%;
+  top: 47.5%;
+}
+.img{
+  width: 1440px;
+}
+
+#completed:checked + #abc{
+  display: block;
+}
+
+.h1text{
+  top: 80px;
+left: 120px;
+width: 160px;
+height: 59px;
+text-align: left;
+font: normal normal medium 40px/59px Noto Sans TC;
+letter-spacing: 0px;
+color: #E8E8E8;
+opacity: 1;
+}
+
+.list-item{
+top: 98px;
+left: 544px;
+width: 48px;
+height: 24px;
+text-align: left;
+font: normal normal normal 16px/24px Noto Sans TC Regular;
+letter-spacing: 0px;
+color: #E8E8E8;
+opacity: 1;
+}
+
+.undonecheck{
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    border: 2px solid #e8e8e8;
+    cursor: pointer;
+}
+.undonetext{
+width: 198px;
+height: 24px;
+font: normal normal normal 16px/24px Noto Sans TC Regular;
+}
+
+.edit{
+background: transparent;
+border: none;
+}
+
+.input-text{
+width: 48px;
+height: 24px;
+text-align: left;
+font: normal normal normal 16px/24px Noto Sans TC Regular;
+letter-spacing: 0px;
+color: #E8E8E8;
+}
 </style>
 
 <script>
+export default {
+  data () {
+    return {
+      num: 1
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
 </script>
