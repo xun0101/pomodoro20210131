baac4800e7a00e8987008569de6c063707975aad
xun <dabby6521@gmail.com>
2022-01-21 16:28:06 +0800

    0121

---
+++ src/components/Chart.vue
@@ -0,0 +1,20 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted () {
+    this.renderchart(this.chartdata, this.options)
+  }
+}
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -57,8 +57,6 @@
   </div>
 </template>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-<script src="https://unpkg.com/vue-chartjs/dist/vue-chartjs.min.js"></script>
 <script>
 export default {
   computed: {
