a764396c2151205588bf8f850b7600b18b8c1498
xun0101 <90738788+xun0101@users.noreply.github.com>
2022-01-23 06:56:46 +0000

    Deploying to gh-pages from @ xun0101/pomodoro20210131@64c31a334a29cc531f6a9e5faff0f66b91876ba3 🚀

---
+++ precache-manifest.ca15f5d66f0eeaf64ddc0cbb0b7aab53.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d16e985399532f11b4",
+    "url": "css/app.191ae91a.css"
+  },
+  {
+    "revision": "2bd4487d0b1b89e45927",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "264002817ea11718023193ff81887242",
+    "url": "img/icon-analysis.26400281.svg"
+  },
+  {
+    "revision": "229b545f1d672cc941fe48443c5b5386",
+    "url": "img/icon-arrow--next.229b545f.svg"
+  },
+  {
+    "revision": "5e5c483b3081ce516c1a89d84c0c7955",
+    "url": "img/icon-arrow--prev.5e5c483b.svg"
+  },
+  {
+    "revision": "670041fc8e70cfc869c440d2b29a9125",
+    "url": "img/icon-bell.670041fc.svg"
+  },
+  {
+    "revision": "d40b7dd6f1257d12cb380c55f957da79",
+    "url": "img/icon-cancel.d40b7dd6.svg"
+  },
+  {
+    "revision": "b2ac0aa4ea01d22a0d601a0c9c9c50ac",
+    "url": "img/icon-delete.b2ac0aa4.svg"
+  },
+  {
+    "revision": "1aebb312b69a14820193cdbd6732ee3f",
+    "url": "img/icon-edit.1aebb312.svg"
+  },
+  {
+    "revision": "7b919d14ddd51420d2fbed702a8244ca",
+    "url": "img/icon-list.7b919d14.svg"
+  },
+  {
+    "revision": "bdbfbb189e248dfeb3e2c2cfcaf63d78",
+    "url": "img/icon-pause--green.bdbfbb18.svg"
+  },
+  {
+    "revision": "d352c4da05c760b8e7ab03e300e9ba38",
+    "url": "img/icon-pause--orange.d352c4da.svg"
+  },
+  {
+    "revision": "d65ccb5b22736e5aa193457e14ccb6e9",
+    "url": "img/icon-play--green.d65ccb5b.svg"
+  },
+  {
+    "revision": "81be6328b5e222bf4873930247d8d4b8",
+    "url": "img/icon-play--orange.81be6328.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6c39a608001b7faba5cd19dbca1bbbfb",
+    "url": "img/tomato--green.6c39a608.svg"
+  },
+  {
+    "revision": "53e02e0bbb45f06dd72830cee7c385a4",
+    "url": "img/tomato--orange.53e02e0b.svg"
+  },
+  {
+    "revision": "dea9842b1b34def05c94e10c636cddbb",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "93e841a58d847ac3fab40a26f715ccef",
+    "url": "index.html"
+  },
+  {
+    "revision": "34d16e985399532f11b4",
+    "url": "js/app.79e1f4d0.js"
+  },
+  {
+    "revision": "2bd4487d0b1b89e45927",
+    "url": "js/chunk-vendors.334d39c6.js"
+  },
+  {
+    "revision": "17e37b39e5b022a31641",
+    "url": "js/list.e3872fc8.js"
+  },
+  {
+    "revision": "a52a79747e03da97d5a9",
+    "url": "js/notfound.cea4fa97.js"
+  },
+  {
+    "revision": "ea741bcc435ffd7c768b",
+    "url": "js/settings.92c7c2a5.js"
+  },
+  {
+    "revision": "c9b40e760532e6899650269eaf023124",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "media/alarm.965b614d.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
